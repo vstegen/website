c180641d142fba05e0f98574f083f8be26909cbe
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2025-07-21 11:35:39 +0900

    Update text on /uses

--- src/app/uses/page.tsx
+++ src/app/uses/page.tsx
@@ -46,8 +46,8 @@ export default function Uses() {
         <ToolsSection title="Workstation">
           <Tool title="14” MacBook Pro, M1 Max, 32GB RAM (2021)">
             Love this machine and even after 4 years, it&apos;s still running
-            everything I throw at it. Might pick a 16&quot; for the larger
-            screen next time.
+            everything I throw at it. At times, I&apos;m certainly missing a
+            slightly larger screen.
           </Tool>
           <Tool title="ZSA Voyager Keyboard">
             Big fan of ZSA keyboards. Had 2 Ergodox and 2 Moonlander Mark I over
@@ -57,8 +57,8 @@ export default function Uses() {
             experience.
           </Tool>
           <Tool title="Logitech MX Master 3S">
-            Ergonomic and silent. It&apos;s a bit inconvenient to carry around
-            due to its size.
+            Ergonomic and silent. Nothing more to say. I can&apos;t use
+            trackpads, so a good mouse is non-negotiable.
           </Tool>
         </ToolsSection>
         <ToolsSection title="Development tools">
@@ -68,26 +68,33 @@ export default function Uses() {
             I use it.
           </Tool>
           <Tool title="Ghostty">
-            Previously used kitty but, after hearing about Ghostty, tried it and
-            have no complains. Easy configuration, fast, native, and has all
-            features I want out of the box.
+            Great native and fast terminal emulator with easy configuration and
+            all the features I need.
           </Tool>
         </ToolsSection>
         <ToolsSection title="Programming Languages">
-          <Tool title="Go">Amazingly productive language.</Tool>
+          <Tool title="Go">
+            Amazingly productive language. Although a bit boring to use (and
+            where are my enums?)
+          </Tool>
           <Tool title="Typescript">
             Your standard language that anyone working in any kind of web
             development should know.
           </Tool>
+          <Tool title="Python">
+            I&apos;ve mostly used Python for my work around Data Science and
+            small scripts.
+          </Tool>
           <Tool title="Rust">
-            I was immediately fascinated by the syntax when I first saw the
-            language on a stream. Currently a work in progress learning Rust.
+            Rust&apos;s syntax fascinated me from the first moment I saw it on
+            Jon Gjengset&apos;s stream. Learning Rust is currently a work in
+            progress.
           </Tool>
           <Tool title="Elixir">
-            Was interested in learning a functional language and the developer
-            satisfaction seemed to be particularly high for Elixir. Phoenix and
-            Liveview look very interesting as well, so let&apos;s see how this
-            one goes.
+            Another work in progress. I was interested in learning a functional
+            language and users of Elixir raved about the developer experience.
+            How could I not try it? Phoenix and Liveview look very interesting
+            as well, so let&apos;s see how this one goes.
           </Tool>
         </ToolsSection>
         <ToolsSection title="Other">
@@ -96,9 +103,14 @@ export default function Uses() {
             integrated clipboard history and more. Don&apos;t want to miss this
             and it&apos;s one of the first things I install on a new Mac.
           </Tool>
+          <Tool title="Little Snitch">
+            The first thing I install on any new machine. Monitoring and
+            controlling network traffic with the option of having multiple
+            profiles for home or travel is something I never want to miss again!
+          </Tool>
           <Tool title="Zen">
-            Recently found this browser. A fan of the vertical tabs and the
-            split screen.
+            Great browser based on Firefox. I&apos;m a big fan of the split
+            screen.
           </Tool>
         </ToolsSection>
       </div>
