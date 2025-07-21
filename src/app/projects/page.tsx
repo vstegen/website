d1c0b288375ee2532fab6f1dc068e1806312436a
vstegen <gitlab.pfH4HeE7@papertrail.fyi>
2025-07-21 11:35:35 +0900

    Update text on /projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -3,54 +3,13 @@ import Image from 'next/image'
 
 import { Card } from '@/components/Card'
 import { SimpleLayout } from '@/components/SimpleLayout'
-import logoAnimaginary from '@/images/logos/animaginary.svg'
-import logoCosmos from '@/images/logos/cosmos.svg'
-import logoHelioStream from '@/images/logos/helio-stream.svg'
-import logoOpenShuttle from '@/images/logos/open-shuttle.svg'
-import logoPlanetaria from '@/images/logos/planetaria.svg'
 
 const projects: {
   name: string
   description: string
   link: { href: string; label: string }
   logo: any // should be an svg file
-}[] = [
-  // {
-  //   name: 'Planetaria',
-  //   description:
-  //     'Creating technology to empower civilians to explore space on their own terms.',
-  //   link: { href: 'http://planetaria.tech', label: 'planetaria.tech' },
-  //   logo: logoPlanetaria,
-  // },
-  // {
-  //   name: 'Animaginary',
-  //   description:
-  //     'High performance web animation library, hand-written in optimized WASM.',
-  //   link: { href: '#', label: 'github.com' },
-  //   logo: logoAnimaginary,
-  // },
-  // {
-  //   name: 'HelioStream',
-  //   description:
-  //     'Real-time video streaming library, optimized for interstellar transmission.',
-  //   link: { href: '#', label: 'github.com' },
-  //   logo: logoHelioStream,
-  // },
-  // {
-  //   name: 'cosmOS',
-  //   description:
-  //     'The operating system that powers our Planetaria space shuttles.',
-  //   link: { href: '#', label: 'github.com' },
-  //   logo: logoCosmos,
-  // },
-  // {
-  //   name: 'OpenShuttle',
-  //   description:
-  //     'The schematics for the first rocket I designed that successfully made it to orbit.',
-  //   link: { href: '#', label: 'github.com' },
-  //   logo: logoOpenShuttle,
-  // },
-]
+}[] = []
 
 function LinkIcon(props: React.ComponentPropsWithoutRef<'svg'>) {
   return (
@@ -104,7 +63,7 @@ export default function Projects() {
   return (
     <SimpleLayout
       title="Things I’ve made trying to put my dent in the universe."
-      intro="I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them are open-source, so if you see something that piques your interest, check out the code and contribute if you have ideas for how it can be improved."
+      intro="A collection of all the things I've worked on over the years."
     >
       {projects.length === 0 ? (
         <EmptyState />
