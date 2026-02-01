8ad8a56d57bb572eaa5dd5cec6d5fe6a8c47ec68
vstegen <github.pQcVWa7e@papertrail.fyi>
2026-02-01 18:52:06 +0900

    Remove logos from projects

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -96,16 +96,6 @@ export default function Projects() {
         >
           {projects.map((project) => (
             <Card as="li" key={project.name}>
-              <div className="relative z-10 flex h-12 w-12 items-center justify-center rounded-full bg-white shadow-md ring-1 shadow-zinc-800/5 ring-zinc-900/5 dark:border dark:border-zinc-700/50 dark:bg-zinc-800 dark:ring-0">
-                {project.logo && (
-                  <Image
-                    src={project.logo}
-                    alt=""
-                    className="h-8 w-8"
-                    unoptimized
-                  />
-                )}
-              </div>
               <h2 className="mt-6 text-base font-semibold text-zinc-800 dark:text-zinc-100">
                 <Card.Link href={project.link.href}>{project.name}</Card.Link>
               </h2>
